24c7728e6890932d3e2f52fb12c6151905d45449
idev-sukanta <sukanta.purkayastha@itlabsolutions.com>
2021-10-23 13:36:07 +0600

    globally middleware updated

--- app/Http/Middleware/DbConnector.php
+++ app/Http/Middleware/DbConnector.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class DbConnector
@@ -20,8 +19,11 @@ class DbConnector
     public function handle(Request $request, Closure $next)
     {
         if(!auth()->check()) return;
+
         Artisan::call('optimize:clear');
+
         Config::set('database.connections.tenant.database',auth()->user()->tenant->database);
+
         Config::set('database.default','tenant');
 
         return $next($request);
